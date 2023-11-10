58b09e518deea00bdd6c7c093b4b50ee97523b10
nicolas sage <nicolas.sage@neoteem.fr>
2023-11-10 09:29:47 +0100

    add

--- src/projetBB.js
+++ src/projetBB.js
@@ -1,24 +1,23 @@
 const vscode = require('vscode');
-const { exec, execSync } = require('child_process');
-
-async function getBitbucketUrl() {
-    let url = await execSync('git config --get remote.origin.url', { encoding: 'utf8' });
-    return url.trim();
-}
+const { exec } = require('child_process');
 
 async function OuvrirProjetBB(context) {
     const folderName = vscode.workspace.workspaceFolders[0].uri.path.split(`/`)[vscode.workspace.workspaceFolders[0].uri.path.split(`/`).length - 1]
-    //const bitbucketUrl = `https://bitbucket.org/neofront/${folderName}/branches/?status=all`
-    //const bitbucketUrlBack = `https://bitbucket.org/neot-v2/${folderName}/branches/?status=all`
+    let config = vscode.workspace.getConfiguration('NeoteemExtension');
+    const nomFront = config.get('urlBBFront');
+    const nomBack = config.get('urlBBBack');
+
+    const bitbucketUrl = `https://bitbucket.org/${nomFront}/${folderName}/branches/?status=all`
+    const bitbucketUrlBack = `https://bitbucket.org/${nomBack}/${folderName}/branches/?status=all`
+
     const disposable = vscode.commands.registerCommand('extension.openBitbucketRepo', async () => {
-        const bitbucketUrlBack = await getBitbucketUrl()
         // Define the Chrome profile you want to use 
-        let config = vscode.workspace.getConfiguration('NeoteemExtension');
         let chromeProfile = config.get('chromeProfile');
         const chromeExecutablePath = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe';
         let chromeCommand
+        const projetsBack = JSON.parse(config.get('nomsBBBack'))
 
-        if (['ws'].includes(folderName)) {
+        if (projetsBack.includes(folderName)) {
             chromeCommand = `"${chromeExecutablePath}" --profile-directory="${chromeProfile}" "${bitbucketUrlBack}"`;
         } else {
             chromeCommand = `"${chromeExecutablePath}" --profile-directory="${chromeProfile}" "${bitbucketUrl}"`;
